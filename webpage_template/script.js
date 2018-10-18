1836bda783ef9dceeffb9c39aa4845f60f498e67
juskevicius <mantas8701@gmail.com>
2018-10-18 17:11:54 +0300

    ugvbh

--- webpage_template/script.js
+++ webpage_template/script.js
@@ -1,19 +1,48 @@
 //controll the contents
 
-function addFormOff() {
-    document.getElementsByClassName("add-form")[0].style.display = "none";
-    document.getElementsByClassName("overlay")[0].style.display = "none";
-}
-function addFormOn() {
-    document.getElementsByClassName("add-form")[0].style.display = "initial";
-    document.getElementsByClassName("overlay")[0].style.display = "initial";
+var onOff1 = true;
+var onOff2 = true;
+
+function showAddForm() {
+    if (onOff1) {
+        document.getElementsByClassName("add-form")[0].style.display = "initial";
+        document.getElementsByClassName("overlay")[0].style.display = "initial";
+    } else {
+        document.getElementsByClassName("add-form")[0].style.display = "none";
+        document.getElementsByClassName("overlay")[0].style.display = "none";
+    }
+    onOff1 = !onOff1;
 }
 
-document.getElementById("offer-group").childNodes[1].style.height = document.getElementById("offer-group").childNodes[3].offsetHeight + "px";
-
-function selectUnit() {
-    event.target.parentElement.parentElement.parentElement.childNodes[3].innerText = event.target.innerText;
+function showOfferGrp() {
+    if (onOff2) {
+        document.getElementsByClassName("offer-group")[0].style.display = "initial";
+    } else {
+        document.getElementsByClassName("offer-group")[0].style.display = "none";
+    }
+    onOff2 = !onOff2;
 }
+
+function selectUnit(nr) {  
+    document.getElementsByClassName("offer-group")[0].getElementsByClassName("dropdown-toggle")[nr].innerHTML = event.target.innerText;
+    createNewOfferElm(nr)
+}
+
+function createNewOfferElm(nr) {
+    let offerToElm = document.getElementsByClassName("offer-elm")[0].cloneNode(true);
+    let offerLabel = offerToElm.getElementsByTagName("label");
+    offerToElm.getElementsByClassName("offer-list")[0].removeChild(offerLabel[0]);
+    offerToElm.getElementsByClassName("weight")[0].removeChild(offerLabel[0]);
+    offerToElm.getElementsByClassName("dropdown-toggle")[0].innerHTML = "Choose <span class='caret'></span>";
+    offerToElm.getElementsByClassName("weight")[0].getElementsByTagName("input")[0].value = "";
+    let units = offerToElm.getElementsByClassName("dropdown-menu")[0].getElementsByTagName("a");
+    for (let i=0; i < units.length; i++) {
+        units[i].setAttribute("onclick", "selectUnit(" + (nr + 1) + ")");
+    }
+    let offerButton = document.getElementsByClassName("offer-btn")[0];
+    document.getElementsByClassName("offer-group")[0].insertBefore(offerToElm, offerButton);
+}
+
 
 // Building the gauge chart:
 
