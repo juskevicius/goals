ddf52313145663950cdfb8d34f82c066cee3620a
juskevicius <mantas8701@gmail.com>
2019-01-07 23:45:17 +0200

    details

--- goal_App/app.js
+++ goal_App/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const cors = require('cors');
 const helmet = require('helmet')
 
@@ -29,7 +28,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: process.env.SECRET1, cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 require('./models/User');
 require('./config/passport');
--- goal_App/controllers/validationController.js
+++ goal_App/controllers/validationController.js
@@ -26,8 +26,8 @@ exports.validate_cookie = [
 ];
 
 exports.user_login_post = [
-  body('empId').isLength({ min: 1 }).trim().withMessage('EmpId empty.'),
-  body('password').isLength({ min: 1 }).trim().withMessage('Password empty.'),
+  body('empId').trim().escape().isLength({ min: 1 }).trim().withMessage('EmpId empty.'),
+  body('password').trim().escape().isLength({ min: 1 }).trim().withMessage('Password empty.'),
   
   sanitizeBody('*').trim().escape(),
 
@@ -49,11 +49,11 @@ exports.user_login_post = [
 
 
 exports.goal_add_post = [
-  body('name').isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
-  body('initScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
-  body('targScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
-  body('comment').isLength({ max: 400 }).trim().withMessage('Comment is too long'),
-  body('task').custom(task => { 
+  body('name').trim().escape().isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
+  body('initScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
+  body('targScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
+  body('comment').trim().escape().isLength({ max: 400 }).trim().withMessage('Comment is too long'),
+  body('task').trim().escape().custom(task => { 
     for (i = 0; i < task.length; i++) {
       if (task[i].description.length > 200 || (task[i].weight && (!isNumeric(task[i].weight) || task[i].weight.toString().length > 11))) {
         return false;
@@ -79,7 +79,7 @@ exports.goal_add_post = [
 ];
 
 exports.goal_details_get = [
-  param('id').custom(id => {
+  param('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
   sanitizeBody('*').trim().escape(),
@@ -99,14 +99,14 @@ exports.goal_details_get = [
 ];
 
 exports.goal_score_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('entryId').trim().optional({ checkFalsy: true }).custom(id => {
+  body('entryId').trim().escape().optional({ checkFalsy: true }).custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('value').trim().isNumeric().isLength({ min: 1, max: 11 }).withMessage('Current score failure'),
-  body('date').trim().isISO8601().withMessage('Date is not valid'),
+  body('value').trim().escape().isNumeric().isLength({ min: 1, max: 11 }).withMessage('Current score failure'),
+  body('date').trim().escape().isISO8601().withMessage('Date is not valid'),
   
   sanitizeBody('*').trim().escape(),
 
@@ -126,10 +126,10 @@ exports.goal_score_post = [
 
 
 exports.goal_editWeight_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('weight').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Current score failure'),
+  body('weight').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Current score failure'),
   
   sanitizeBody('*').trim().escape(),
 
@@ -149,15 +149,15 @@ exports.goal_editWeight_post = [
 
 
 exports.goal_taskImplementation_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('taskId').custom(id => {
+  body('taskId').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('Task id is not valid'),
-  body('implemented').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Implementation score failure'),
-  body('weight').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('weight failure'),
-  body('description').isLength({ min: 1, max: 200 }).trim().withMessage('Task description failure. Max length is 200 sybmols.'),
+  body('implemented').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Implementation score failure'),
+  body('weight').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('weight failure'),
+  body('description').trim().escape().isLength({ min: 1, max: 200 }).withMessage('Task description failure. Max length is 200 sybmols.'),
   
   sanitizeBody('*').trim().escape(),
 
@@ -185,13 +185,13 @@ exports.goal_taskImplementation_post = [
 // Handle Goal edit on POST.
 
 exports.goal_edit_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('name').isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
-  body('initScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
-  body('targScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
-  body('comment').isLength({ max: 400 }).trim().withMessage('Comment is too long'),
+  body('name').trim().escape().isLength({ min: 1, max: 100 }).withMessage('Goal failure. Max length is 100 sybmols.'),
+  body('initScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
+  body('targScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
+  body('comment').trim().escape().isLength({ max: 400 }).withMessage('Comment is too long'),
   body('task').custom(task => { 
     for (i = 0; i < task.length; i++) {
       if (task[i].description.length > 200 || (task[i].weight && (!isNumeric(task[i].weight) || task[i].weight.toString().length > 11))) {
@@ -220,7 +220,7 @@ exports.goal_edit_post = [
 // Handle Goal delete on POST.
 
 exports.goal_delete_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
   sanitizeBody('*').trim().escape(),
@@ -243,15 +243,15 @@ exports.goal_delete_post = [
 
 exports.goal_offerTo_post = [
   
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('name').isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
-  body('offers.*.owner').custom(id => { return id.match(/^[a-fA-F0-9]{24}$/) ? true : false; }).withMessage('owner id is not valid'),
-  body('offers.*.initScore').optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
-  body('offers.*.targScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
-  body('offers.*.comment').isLength({ max: 400 }).trim().withMessage('Comment is too long'),
-  body('offers.*.task').custom(task => { 
+  body('name').trim().escape().isLength({ min: 1, max: 100 }).withMessage('Goal failure. Max length is 100 sybmols.'),
+  body('offers.*.owner').trim().escape().custom(id => { return id.match(/^[a-fA-F0-9]{24}$/) ? true : false; }).withMessage('owner id is not valid'),
+  body('offers.*.initScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
+  body('offers.*.targScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
+  body('offers.*.comment').trim().escape().isLength({ max: 400 }).withMessage('Comment is too long'),
+  body('offers.*.task').trim().escape().custom(task => { 
     for (i = 0; i < task.length; i++) {
       if (task[i].description.length > 200 || (task[i].weight && (!isNumeric(task[i].weight) || task[i].weight.toString().length > 11))) {
         return false;
@@ -279,7 +279,7 @@ exports.goal_offerTo_post = [
 // Handle Goal accept offer on POST.
 
 exports.goal_acceptOffer_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
   sanitizeBody('*').trim().escape(),
@@ -300,14 +300,14 @@ exports.goal_acceptOffer_post = [
 // Handle Goal owners offer on POST.
 
 exports.goal_ownersOffer_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('name').isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
-  body('initScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
-  body('targScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
-  body('comment').isLength({ max: 400 }).trim().withMessage('Comment is too long'),
-  body('task').custom(task => { 
+  body('name').trim().escape().isLength({ min: 1, max: 100 }).withMessage('Goal failure. Max length is 100 sybmols.'),
+  body('initScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
+  body('targScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
+  body('comment').trim().escape().isLength({ max: 400 }).withMessage('Comment is too long'),
+  body('task').trim().escape().custom(task => { 
     for (i = 0; i < task.length; i++) {
       if (task[i].description.length > 200 || (task[i].weight && (!isNumeric(task[i].weight) || task[i].weight.toString().length > 11))) {
         return false;
@@ -340,14 +340,14 @@ exports.goal_ownersOffer_post = [
 // Handle Goal approvers offer on POST.
 
 exports.goal_approversOffer_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
-  body('name').isLength({ min: 1, max: 100 }).trim().withMessage('Goal failure. Max length is 100 sybmols.'),
-  body('initScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
-  body('targScore').trim().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
-  body('comment').isLength({ max: 400 }).trim().withMessage('Comment is too long'),
-  body('task').custom(task => { 
+  body('name').trim().escape().isLength({ min: 1, max: 100 }).withMessage('Goal failure. Max length is 100 sybmols.'),
+  body('initScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Initial score failure'),
+  body('targScore').trim().escape().optional({ checkFalsy: true }).isNumeric().isLength({ max: 11 }).withMessage('Target score failure'),
+  body('comment').trim().escape().isLength({ max: 400 }).withMessage('Comment is too long'),
+  body('task').trim().escape().custom(task => { 
     for (i = 0; i < task.length; i++) {
       if (task[i].description.length > 200 || (task[i].weight && (!isNumeric(task[i].weight) || task[i].weight.toString().length > 11))) {
         return false;
@@ -375,7 +375,7 @@ exports.goal_approversOffer_post = [
 // Handle Goal approve offer on POST.
 
 exports.goal_approve_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
   sanitizeBody('*').trim().escape(),
@@ -396,7 +396,7 @@ exports.goal_approve_post = [
 // Handle Goal reject on POST.
 
 exports.goal_reject_post = [
-  body('id').custom(id => {
+  body('id').trim().escape().custom(id => {
     return id.match(/^[a-fA-F0-9]{24}$/) ? true : false;
   }).withMessage('id is not valid'),
   sanitizeBody('*').trim().escape(),
