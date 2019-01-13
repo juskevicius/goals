449bfdd9c4ed369eba3f9a804dca2df8f0dc13e9
Mantas <mantas8701@gmail.com>
2019-01-13 22:00:25 +0200

    moved up

--- app.js
+++ app.js
@@ -1,72 +1,66 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet')
 
-var express = require("express");
-var app = express();
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(__dirname + '/webpage_template'));
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
 
-var port = 27017;
+const app = express();
 
-// Connection URL
-const url = 'mongodb://localhost:27017/goals';
+//Set up mongoose connection.
+const mongoose = require('mongoose');
+const mongoDB = process.env.DB_ADDRESS;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Database Name
-const dbName = 'Goals';
- 
-// Use connect method to connect to the server
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  if (err) throw err;
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+app.use(helmet());
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-  const db = client.db(dbName);
-   
-  app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/webpage_template/index.html')
-  });
-
-  app.get("/add", (req, res) => {
-    res.sendFile(__dirname + '/webpage_template/add.html')
-  });
-
-  app.post("/addAGoal", (req, res) => {
-    db.collection("goalList").insertOne(req.body, (err, result) => {
-      if (err) throw err;
-      console.log("saved to DB");
-      /*res.redirect('/add');*/
-      db.collection("goalList").find().toArray(function(err, documents) {
-        if (err) throw err;
-        console.log(documents);
-      });
-    });
-  });
-
-  app.get("/displayAll", (req, res) => {
-    
-    db.collection("goalList").find().toArray(function(err, documents) {
-      if (err) throw err;
-      res.send(documents);
-    });
-  });
-
-   
-  app.listen(port, () => {
-   console.log("Server listening on port " + port);
-  });
-
-  /*
-  db.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-
-    db.listCollections().toArray(function(err, collections){
-      console.log(collections);
-      client.close();
-    });
-
-  });*/
-
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
-  
+
+require('./models/User');
+require('./config/passport');
+app.use(require('./routes/users'));
+app.use(require('./routes/goals'));
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      },
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+module.exports = app;
